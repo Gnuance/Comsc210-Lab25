b34b9164d00bc988727a8e2bec89d8a8c626babb
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-10-27 13:58:45 -0700

    10 minute timestamp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
+/*
+    Lab 25: Which data structure is fastest? In each race, the contestants (a vector, list, and set) will all compete against each other.
+        1. There will be four various races! Write a program that will time, record, and report how long it takes each contestant to run these four races:
+            a. The first race is READING. The challenge is to read the 20,000 data elements into each of these data structures: vector, list, set
+            b. The second race is SORTING. Sort the vector and sort the list. Set the set's value to -1 since a set is already sorted.
+            c. The third race is INSERTING. Insert the value "TESTCODE" into the middle of the vector or the middle of the list, or into the set
+            d. The fourth race is DELETING. Delete the middle-ish element of the vector, the list, or the set
+        2. Use the cool time features of the <chrono> library. You'll just be using a stopwatch on these operations.
+    Requirements:
+        1. Make your code scalable. It should be simple to add new races.
+*/
+
 #include <iostream>
 #include <chrono>
+// required data classes
+#include <vector>
+#include <list>
+#include <set>
 using namespace std;
+
+// templated function to read data into whatever container is passed
+template <typename T>
+void ReadDataInto(T &container, const string fileName);
 
 int main() {
     
